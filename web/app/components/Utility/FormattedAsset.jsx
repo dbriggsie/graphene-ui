58839904f4afb772a07de8ae4de35313fcd93c27
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-12-28 11:11:09 +0100

    Use 2 decimals minimum in FormattedAsset

--- web/app/components/Utility/FormattedAsset.jsx
+++ web/app/components/Utility/FormattedAsset.jsx
@@ -112,7 +112,7 @@ class FormattedAsset extends React.Component {
                 {!hide_amount ?
                     <FormattedNumber
                         value={this.props.exact_amount ? amount : amount / precision}
-                        minimumFractionDigits={0}
+                        minimumFractionDigits={2}
                         maximumFractionDigits={decimals}
                     />
                 : null}
