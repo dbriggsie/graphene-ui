f0068069ed577e8454d8b4f6a6f4477f8eabc31c
OpenLedgerDev <dt@ccedk.com>
2017-02-15 15:02:47 +0300

    remove promise polyphill

--- web/app/Main.js
+++ web/app/Main.js
@@ -1,20 +1,7 @@
 require("file-loader?name=dictionary.json!common/dictionary_en.json");
-
-//require("whatwg-fetch");
-//require("indexeddbshim");
-//require("./assets/locales/locales.js");
-
-
-//require("./stylesheets/app.scss");
-//require("file-loader?name=index.html!./index.html");
-require("file-loader?name=dictionary.json!common/dictionary_en.json");
-require("babel-polyfill");
 require("whatwg-fetch");
 require("indexeddbshim");
-//require("./asset-symbols/symbols.js");
 require("./assets/locales/locales.js");
-
-
 
 if (!window.Intl) { // Safari polyfill
     require.ensure(["intl"], require => {
