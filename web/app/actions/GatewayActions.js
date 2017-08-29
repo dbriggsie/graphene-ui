12f6df9be21cfa706227f48ad88e919cab9055f5
alexandr <alexandrtisa@gmail.com>
2017-08-29 10:26:49 +0300

    bug bridge

--- web/app/actions/GatewayActions.js
+++ web/app/actions/GatewayActions.js
@@ -1,29 +1,37 @@
 import alt from "alt-instance";
 import { fetchCoins, fetchBridgeCoins, getBackedCoins, getActiveWallets } from "common/blockTradesMethods";
-import { blockTradesAPIs } from "api/apiConfig";
+import {blockTradesAPIs} from "api/apiConfig";
 
-let inProgress = {};  
+let inProgress = {};
 
 class GatewayActions {
 
-    fetchCoins({ backer, url } = {}) {
-        return (dispatch) => {
-            Promise.all([
-                fetchCoins(url),
-                getActiveWallets(blockTradesAPIs.BASE_OL + blockTradesAPIs.ACTIVE_WALLETS)
-            ]).then(result => {
-                let [coins, wallets] = result;
-                                    //coins = test;
-                dispatch({
-                    coins: coins,
-                    backedCoins: getBackedCoins({ allCoins: coins, backer: backer }).filter(a => {
-                        return wallets.indexOf(a.walletType) !== -1;
-                    }),
-                    backer
+
+    fetchCoins({backer = "OPEN", url = undefined} = {}) {
+        if (!inProgress["fetchCoins_" + backer]) {
+            inProgress["fetchCoins_" + backer] = true;
+            return (dispatch) => {
+                Promise.all([fetchCoins(url),
+                    fetchBridgeCoins(blockTradesAPIs.BASE_OL + blockTradesAPIs.COINS_LIST),
+                    getActiveWallets(blockTradesAPIs.BASE_OL + blockTradesAPIs.ACTIVE_WALLETS)
+                ]).then(result => {
+                    delete inProgress["fetchCoins_" + backer];
+                    let [coins, tradingPairs, wallets] = result;
+                    dispatch({
+                        coins: coins,
+                        backedCoins: getBackedCoins({allCoins: coins, tradingPairs: tradingPairs, backer: backer}).filter(a => {
+                            return wallets.indexOf(a.walletType) !== -1;
+                        }),
+                        backer
+                    });
                 });
-            });
-        };
+            };
+        } else {
+            return {};
+        }
     }
+
+
 
     fetchBridgeCoins(url = undefined) {
         if (!inProgress["fetchBridgeCoins"]) {
@@ -31,8 +39,8 @@ class GatewayActions {
             return (dispatch) => {
                 Promise.all([
                     fetchCoins(url),
-                    fetchBridgeCoins(url),
-                    getActiveWallets(blockTradesAPIs.BASE + blockTradesAPIs.ACTIVE_WALLETS)
+                    fetchBridgeCoins(blockTradesAPIs.BASE + blockTradesAPIs.TRADING_PAIRS),
+                    getActiveWallets(blockTradesAPIs.BASE_OL + blockTradesAPIs.ACTIVE_WALLETS)
                 ]).then(result => {
                     delete inProgress["fetchBridgeCoins"];
                     let [coins, bridgeCoins, wallets] = result;
@@ -51,107 +59,3 @@ class GatewayActions {
 
 export default alt.createActions(GatewayActions);
 
-let test = [
-  {
-    "coinType": "open.ltc",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger Litecoin",
-    "symbol": "OPEN.LTC",
-    "walletSymbol": "OPEN.LTC",
-    "walletType": "bitshares2",
-    "transactionFee": 0,
-    "precision": 100000000,
-    "backingCoinType": "ltc",
-    "supportsOutputMemos": true,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null,
-    "gateFee": 0.003,
-    "intermediateAccount": "openledger-wallet"
-  },
-    {
-    "coinType": "openltc",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger Litecoin (dep.)",
-    "symbol": "OPENLTC",
-    "walletSymbol": "OPENLTC",
-    "walletType": "bitshares2",
-    "transactionFee": 0,
-    "precision": 100000000,
-    "backingCoinType": "ltc",
-    "supportsOutputMemos": true,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null,
-    "gateFee": 0.003,
-    "intermediateAccount": "openledger-wallet"
-  },
-    {
-    "coinType": "ltc",
-    "walletName": "Litecoin",
-    "name": "Litecoin",
-    "symbol": "LTC",
-    "walletSymbol": "LTC",
-    "walletType": "litecoin",
-    "transactionFee": 0.003,
-    "precision": 100000000,
-    "backingCoinType": null,
-    "supportsOutputMemos": false,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null,
-    "gateFee": 0.003,
-    "intermediateAccount": "openledger-wallet"
-  },
-  {
-    "coinType": "open.btc",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger Bitcoin",
-    "symbol": "OPEN.BTC",
-    "walletSymbol": "OPEN.BTC",
-    "walletType": "bitshares2",
-    "transactionFee": 0,
-    "precision": 100000000,
-    "backingCoinType": "btc",
-    "supportsOutputMemos": true,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null,
-    "gateFee": 0.0003,
-    "intermediateAccount": "openledger-wallet"
-  },
-  {
-    "coinType": "openbtc",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger Bitcoin (dep.)",
-    "symbol": "OPENBTC",
-    "walletSymbol": "OPENBTC",
-    "walletType": "bitshares2",
-    "transactionFee": 0,
-    "precision": 100000000,
-    "backingCoinType": "btc",
-    "supportsOutputMemos": true,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null,
-    "gateFee": "0.00030",
-    "intermediateAccount": "openledger-wallet"
-  },
-    {
-    "coinType": "btc",
-    "walletName": "Bitcoin",
-    "name": "Bitcoin",
-    "symbol": "BTC",
-    "walletSymbol": "BTC",
-    "walletType": "bitcoin",
-    "transactionFee": 0.0003,
-    "precision": 100000000,
-    "backingCoinType": null,
-    "supportsOutputMemos": false,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null,
-    "gateFee": 0.0003,
-    "intermediateAccount": "openledger-wallet"
-  }
-];
