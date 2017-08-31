3453f05bb337bc701bf3ca90677a34fd6b714b6e
Da99er <da99er87@ya.ru>
2017-08-31 11:19:22 +0300

    fix bridge

--- web/app/actions/GatewayActions.js
+++ web/app/actions/GatewayActions.js
@@ -1,29 +1,36 @@
 import alt from "alt-instance";
 import { fetchCoins, fetchBridgeCoins, getBackedCoins, getActiveWallets } from "common/blockTradesMethods";
-import { blockTradesAPIs } from "api/apiConfig";
+import {blockTradesAPIs} from "api/apiConfig";
 
-let inProgress = {};  
+let inProgress = {};
 
 class GatewayActions {
 
-    fetchCoins({ backer, url } = {}) {
-        return (dispatch) => {
-            Promise.all([
-                fetchCoins(url),
-                getActiveWallets(blockTradesAPIs.BASE_OL + blockTradesAPIs.ACTIVE_WALLETS)
-            ]).then(result => {
-                let [coins, wallets] = result;
-                                   // coins = test;
-                dispatch({
-                    coins: coins,
-                    backedCoins: getBackedCoins({ allCoins: coins, backer: backer }).filter(a => {
-                        return wallets.indexOf(a.walletType) !== -1;
-                    }),
-                    backer
+    fetchCoins({backer = "OPEN", url = undefined} = {}) {
+        if (!inProgress["fetchCoins_" + backer]) {
+            inProgress["fetchCoins_" + backer] = true;
+            return (dispatch) => {
+                Promise.all([fetchCoins(url),
+                    fetchBridgeCoins(blockTradesAPIs.BASE_OL + blockTradesAPIs.COINS_LIST),
+                    getActiveWallets(blockTradesAPIs.BASE_OL + blockTradesAPIs.ACTIVE_WALLETS)
+                ]).then(result => {
+                    delete inProgress["fetchCoins_" + backer];
+                    let [coins, tradingPairs, wallets] = result;
+                    dispatch({
+                        coins: coins,
+                        backedCoins: getBackedCoins({allCoins: coins, tradingPairs: tradingPairs, backer: backer}).filter(a => {
+                            return wallets.indexOf(a.walletType) !== -1;
+                        }),
+                        backer
+                    });
                 });
-            });
-        };
+            };
+        } else {
+            return {};
+        }
     }
+
+
 
     fetchBridgeCoins(url = undefined) {
         if (!inProgress["fetchBridgeCoins"]) {
@@ -31,8 +38,8 @@ class GatewayActions {
             return (dispatch) => {
                 Promise.all([
                     fetchCoins(url),
-                    fetchBridgeCoins(url),
-                    getActiveWallets(blockTradesAPIs.BASE + blockTradesAPIs.ACTIVE_WALLETS)
+                    fetchBridgeCoins(blockTradesAPIs.BASE + blockTradesAPIs.TRADING_PAIRS),
+                    getActiveWallets(blockTradesAPIs.BASE_OL + blockTradesAPIs.ACTIVE_WALLETS)
                 ]).then(result => {
                     delete inProgress["fetchBridgeCoins"];
                     let [coins, bridgeCoins, wallets] = result;
@@ -51,110 +58,3 @@ class GatewayActions {
 
 export default alt.createActions(GatewayActions);
 
-let test = [
-  {
-    "coinType": "open.nbt",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger NuBits",
-    "symbol": "OPEN.NBT",
-    "walletSymbol": "OPEN.NBT",
-    "walletType": "bitshares2",
-    "transactionFee": "0.",
-    "precision": "100000000.",
-    "backingCoinType": "nbt",
-    "supportsOutputMemos": true,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  },
-    {
-    "coinType": "nbt",
-    "walletName": "NuBits",
-    "name": "NuBits",
-    "symbol": "NBT",
-    "walletSymbol": "NBT",
-    "walletType": "nubits",
-    "transactionFee": "0.01",
-    "precision": "100000000.",
-    "backingCoinType": null,
-    "supportsOutputMemos": false,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  },
-  {
-    "coinType": "nsr",
-    "walletName": "NuShares",
-    "name": "NuShares",
-    "symbol": "NSR",
-    "walletSymbol": "NSR",
-    "walletType": "nushares",
-    "transactionFee": "1.",
-    "precision": "100000000.",
-    "backingCoinType": null,
-    "supportsOutputMemos": false,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  }, 
-  {
-    "coinType": "etp",
-    "walletName": "BitShares 2.0",
-    "name": "Metaverse Entropy",
-    "symbol": "ETP",
-    "walletSymbol": "ETP",
-    "walletType": "bitshares2",
-    "transactionFee": "0",
-    "precision": "100000000",
-    "backingCoinType": null,    
-    "supportsOutputMemos": false,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  },
-    {
-    "coinType": "open.nsr",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger NuShares (deprecated)",
-    "symbol": "OPEN.NSR",
-    "walletSymbol": "OPEN.NSR",
-    "walletType": "bitshares2",
-    "transactionFee": "0.",
-    "precision": "100000000.",
-    "backingCoinType": "nsr",
-    "supportsOutputMemos": true,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  },
-    {
-    "coinType": "etp",
-    "walletName": "BitShares 2.0",
-    "name": "Metaverse Entropy",
-    "symbol": "ETP",
-    "walletSymbol": "ETP",
-    "walletType": "etp", //bitshares2
-    "transactionFee": "0",
-    "precision": "100000000",
-    "backingCoinType": null,    
-    "supportsOutputMemos": false,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  },
-  {
-    "coinType": "open.etp",
-    "walletName": "BitShares 2.0",
-    "name": "OpenLedger Metaverse Entropy",
-    "symbol": "OPEN.ETP",
-    "walletSymbol": "OPEN.ETP",
-    "walletType": "bitshares2",
-    "transactionFee": "0",
-    "precision": "100000000",
-    "backingCoinType": "etp",
-    "supportsOutputMemos": 1,
-    "restricted": false,
-    "authorized": null,
-    "notAuthorizedReasons": null
-  }
-];
