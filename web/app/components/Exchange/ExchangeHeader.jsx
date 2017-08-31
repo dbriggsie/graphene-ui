72423e3332724a0a297f059e50db136b4a3e7cb7
Da99er <da99er87@ya.ru>
2017-08-31 16:33:09 +0300

    add settlement

--- web/app/components/Exchange/ExchangeHeader.jsx
+++ web/app/components/Exchange/ExchangeHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router/es";
+import {Link} from "react-router/es";
 import Icon from "../Icon/Icon";
 import AssetName from "../Utility/AssetName";
 import MarketsActions from "actions/MarketsActions";
@@ -7,13 +7,9 @@ import SettingsActions from "actions/SettingsActions";
 import PriceStat from "./PriceStat";
 import Translate from "react-translate-component";
 import counterpart from "counterpart";
-import { debounce, compose, property } from "lodash";
+import cnames from "classnames";
 
 export default class ExchangeHeader extends React.Component {
-    constructor() {
-        super();
-    }
-
     shouldComponentUpdate(nextProps) {
         if (!nextProps.marketReady) return false;
         return true;
@@ -29,27 +25,10 @@ export default class ExchangeHeader extends React.Component {
     }
 
     render() {
-        const {
-            quoteAsset,
-            baseAsset,
-            starredMarkets,
-            hasPrediction,
-            feedPrice,
-            showCallLimit,
-            lowestCallPrice,
-            marketReady,
-            latestPrice,
-            currentPeriod,
-            marketStats,
-            showDepthChart,
-            buckets,
-            bucketSize,
-            showIndicators,
-            onBorrowBase,
-            onBorrowQuote,
-            indicators,
-            indicatorSettings
-        } = this.props;
+        const {quoteAsset, baseAsset, starredMarkets, hasPrediction, feedPrice,
+            showCallLimit, lowestCallPrice, marketReady, latestPrice, currentPeriod,
+            marketStats, showDepthChart, buckets, bucketSize, showIndicators,
+            onBorrowBase, onBorrowQuote, indicators, indicatorSettings} = this.props;
 
         const baseSymbol = baseAsset.get("symbol");
         const quoteSymbol = quoteAsset.get("symbol");
@@ -66,49 +45,54 @@ export default class ExchangeHeader extends React.Component {
         const volumeBase = marketStats.get("volumeBase");
         const volumeQuote = marketStats.get("volumeQuote");
 
-        let dayChangeArrowSymbol = dayChangeArrow === "" ? null : dayChangeArrow === "change-up" ? <span>&#8593;</span> : <span>&#8595;</span>;
-
         return (
-            <div className="grid-block shrink top-bar">
-				<div className="grid-block shrink market-symbol">
-					<span onClick={this._addMarket.bind(this, quoteSymbol, baseSymbol)} className="market-symbol">
-						<Icon className={starClass} name="fi-star"/>
-					</span>
-					{!hasPrediction ? (
-					<Link onClick={() => {MarketsActions.switchMarket();}} className="market-symbol" to={`/market/${baseSymbol}_${quoteSymbol}`}>
-						<span><AssetName name={quoteSymbol} replace={true} /> &#8660; <AssetName name={baseSymbol} replace={true} /></span>
-					</Link>
-                    ) : (
-					<a>{`${quoteSymbol} : ${baseSymbol}`}</a>					
-					)}
-				</div>
+            <div className="grid-block shrink overflow-visible top-bar">
+                <div className="grid-block overflow-visible">
+                    <div className="grid-block shrink show-for-large" >
+                        <div className="v-align">
+                        <span style={{paddingRight: 0}} onClick={this._addMarket.bind(this, quoteSymbol, baseSymbol)} className="market-symbol">
+                            <Icon className={starClass} name="fi-star"/>
+                        </span>
+                            {!hasPrediction ? (
+                                <Link onClick={() => {MarketsActions.switchMarket();}} className="market-symbol" to={`/market/${baseSymbol}_${quoteSymbol}`}>
+                                    <span><AssetName name={quoteSymbol} replace={true} /> : <AssetName name={baseSymbol} replace={true} /></span>
+                                </Link>) : (
+                                <a className="market-symbol">
+                                    <span>{`${quoteSymbol} : ${baseSymbol}`}</span>
+                                </a>
+                            )}
+                        </div>
+                    </div>
 
-					<div className="grid-block vertical" style={{overflow: "visible"}}>
-						<div className="grid-block wrap market-stats-container">
-							<ul className="market-stats stats top-stats">
-								{latestPrice ? <PriceStat ready={marketReady} price={latestPrice.full} quote={quoteAsset} base={baseAsset} content="exchange.latest"/> : null}
-								<li className="stat" >
-									<span>
-										<Translate component="span" content="account.hour_24" />:&nbsp;
-										<b className={"value " + dayChangeClass}>{marketReady ? dayChange : 0}<span className={dayChangeArrow}>% {dayChangeArrowSymbol}</span></b>		
-									</span>
-								</li>
+                    <div className="grid-block vertical" style={{overflow: "visible"}}>
+                        <div className="grid-block wrap market-stats-container">
+                            <ul className="market-stats stats top-stats">
+                                {latestPrice ?
+                                    <PriceStat ready={marketReady} price={latestPrice.full} quote={quoteAsset} base={baseAsset} content="exchange.latest"/> : null}
 
-								{(volumeBase >= 0) ? <PriceStat ready={marketReady} decimals={0} volume={true} price={volumeBase} className="column-hide-small" volume2={volumeQuote} base={baseAsset} quote={quoteAsset} content="exchange.volume_24"/> : null}
+                                <li className="stat">
+                                    <span>
+                                        <Translate component="span" content="account.hour_24" />
+                                        <b className={"value " + dayChangeClass}>{marketReady ? dayChange : 0}<span className={dayChangeArrow}>&nbsp;{dayChangeArrow === "" ? null : dayChangeArrow === "change-up" ? <span>&#8593;</span> : <span>&#8595;</span>}</span></b>
+                                        <span>%</span>
+                                    </span>
+                                </li>
 
-								{false && !hasPrediction && feedPrice ?
-									<PriceStat toolTip={counterpart.translate("tooltip.settle_price")} ready={marketReady} className="column-hide-small" price={feedPrice.toReal()} quote={quoteAsset} base={baseAsset} content="exchange.settle"/> : null}
+                                {(volumeBase >= 0) ? <PriceStat ready={marketReady} decimals={0} volume={true} price={volumeBase} className="column-hide-small" volume2={volumeQuote} base={baseAsset} quote={quoteAsset} content="exchange.volume_24"/> : null}
 
-								{false && lowestCallPrice && showCallLimit ?
-									<PriceStat toolTip={counterpart.translate("tooltip.call_limit")} ready={marketReady} className="column-hide-medium is-call" price={lowestCallPrice} quote={quoteAsset} base={baseAsset} content="explorer.block.call_limit"/> : null}
+                                {!hasPrediction && feedPrice ?
+                                    <PriceStat toolTip={counterpart.translate("tooltip.settle_price")} ready={marketReady} className="column-hide-small" price={feedPrice.toReal()} quote={quoteAsset} base={baseAsset} content="exchange.settle"/> : null}
 
-								{false && feedPrice && showCallLimit ?
-									<PriceStat toolTip={counterpart.translate("tooltip.margin_price")} ready={marketReady} className="column-hide-medium is-call" price={feedPrice.getSqueezePrice({real: true})} quote={quoteAsset} base={baseAsset} content="exchange.squeeze"/> : null}
-							</ul>
-						</div>
-					</div>
+                                {lowestCallPrice && showCallLimit ?
+                                    <PriceStat toolTip={counterpart.translate("tooltip.call_limit")} ready={marketReady} className="column-hide-medium is-call" price={lowestCallPrice} quote={quoteAsset} base={baseAsset} content="explorer.block.call_limit"/> : null}
 
-			</div>
+                                {feedPrice && showCallLimit ?
+                                    <PriceStat toolTip={counterpart.translate("tooltip.margin_price")} ready={marketReady} className="column-hide-medium is-call" price={feedPrice.getSqueezePrice({real: true})} quote={quoteAsset} base={baseAsset} content="exchange.squeeze"/> : null}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
