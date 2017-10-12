ac36e327365deef0719220343f20b9a6151e705c
OpenLedgerDev <dt@ccedk.com>
2017-10-12 13:59:50 +0300

    hide olLightTheme

--- web/app/stores/SettingsStore.js
+++ web/app/stores/SettingsStore.js
@@ -200,7 +200,7 @@ class SettingsStore {
                 "darkTheme",
                 "lightTheme",
                 "olDarkTheme",
-                "olLightTheme"
+                //"olLightTheme"
             ],
             passwordLogin: [
                 {translate: "yes"},
